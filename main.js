45605d5d5b45ce4d48dee67e103057f3e565dac0
Johan Lidberg <johan.lidberg@skola.botkyrka.se>
2021-01-18 15:55:34 +0100

    Hello

--- main.js
+++ main.js
@@ -1,29 +1,3 @@
-function caesarEncrypt(msg, shift) {
+import { caesarEncrypt, caeserDecrypt } from "./diycrypto.js"
 
-    const charCodes = []
-    for (let i = 0; i < msg.length; i++) {
-        charCodes.push(msg[i].charCodeAt(0))
-    }
-
-    for (let i = 0; i < charCodes.length; i++) {
-        charCodes[i] = charCodes[i] + shift
-    }
-
-    let reslut = ""
-    for (let i = 0; i < charCodes.length; i++) {
-        result = result + String.fromCharCode(charCodes[i])
-    }
-    
-    return result
-}
-
-function caeserDecrypt(encryptedMsg, shift) {
-    for (let i = 0; i < charCodes.length; index++) {
-           caesarEncrypt = caesarEncrypt - shift     
-    }
-}
-const secretMessage = "ATTACK"
-const encryptedMessage = caesarEncrypt(secretMessage, 4)
-
-
-console.log(secretMessage, encryptedMessage, decryptedMessage)
+console.log(caesarEncrypt("HELLO", 1))
